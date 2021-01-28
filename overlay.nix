e81f8dcf0095ac2e323ffbce96935e40054dc479
Basile Henry <bjm.henry@gmail.com>
2021-01-28 11:22:11 +0100

    Update cargoSha256

--- overlay.nix
+++ overlay.nix
@@ -5,7 +5,7 @@ final: prev:
       pname = "prjfmt";
       version = "0.0.1";
       src = ./.;
-      cargoSha256 = "sha256-98M9OPWil9bKknam8ys4dNP6/iZObW0RrAC7PxiHxYI=";
+      cargoSha256 = "sha256:0bsxwl5bhjg8d4mdyk2mx7g5744b790nhdlyg393vwjmnbnyy1k2";
       doCheck = false;
       nativeBuildInputs = [ ];
       buildInputs = [ ];
