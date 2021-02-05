9d9509776b4425e4cc9a548d7ec32b4b8b010e2a
zimbatm <zimbatm@zimbatm.com>
2021-02-05 23:31:30 +0100

    replace 'which' with crate
    The binary is not available on all platforms.

--- overlay.nix
+++ overlay.nix
@@ -5,7 +5,7 @@ final: prev:
       pname = "prjfmt";
       version = "0.0.1";
       src = ./.;
-      cargoSha256 = "sha256:0bsxwl5bhjg8d4mdyk2mx7g5744b790nhdlyg393vwjmnbnyy1k2";
+      cargoSha256 = "1n9wzbv6nc5sh2l0j1xfb2z65ql2krqw2ba9s7krlfhsbdkndwrr";
       doCheck = false;
       nativeBuildInputs = [ ];
       buildInputs = [ ];
