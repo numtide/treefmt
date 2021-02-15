c2641b429a52ebdd0cb8b170e400be4d3e43d57b
Andika Demas Riyandi <andika.riyan@gmail.com>
2021-02-15 20:20:05 +0700

    implement lookup function to find prjfmt.toml (#52)

--- src/command/format.rs
+++ src/command/format.rs
@@ -1,18 +1,19 @@
-use super::Cli;
+use super::lookup_prjfmt_toml;
 use crate::engine::run_prjfmt;
 use crate::CLOG;
 use anyhow::anyhow;
-use std::env;
+use std::{env, path::PathBuf};
 use std::fs;
 use std::path::Path;
 
-pub fn format_cmd(cli: Cli) -> anyhow::Result<()> {
-    let cwd = match cli.files {
-        Some(path) => path,
-        None => env::current_dir()?,
+pub fn format_cmd(path: Option<PathBuf>) -> anyhow::Result<()> {
+    let cwd = env::current_dir()?;
+    let cfg_dir = match path {
+        Some(p) => p,
+        None => lookup_prjfmt_toml(cwd)?,
     };
 
-    let prjfmt_toml = cwd.join("prjfmt.toml");
+    let prjfmt_toml = cfg_dir.join("prjfmt.toml");
     let xdg_cache_dir = match env::var("XDG_CACHE_DIR") {
         Ok(path) => path,
         Err(err) => {
@@ -35,12 +36,12 @@ pub fn format_cmd(cli: Cli) -> anyhow::Result<()> {
         CLOG.debug(&format!(
             "Found {} at {}",
             prjfmt_toml.display(),
-            cwd.display()
+            cfg_dir.display()
         ));
-        CLOG.debug(&format!("Change current directory into: {}", cwd.display()));
+        CLOG.debug(&format!("Change current directory into: {}", cfg_dir.display()));
         let cache_dir = Path::new(&xdg_cache_dir).join("prjfmt/eval-cache");
         fs::create_dir_all(&cache_dir)?;
-        run_prjfmt(cwd, cache_dir)?;
+        run_prjfmt(cfg_dir, cache_dir)?;
     } else {
         CLOG.error(
             "file prjfmt.toml couldn't be found. Run `--init` to generate the default setting",
