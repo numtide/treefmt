4991a97d3733c6e77b9aa56335ad02223c857fd6
zimbatm <zimbatm@zimbatm.com>
2021-02-25 14:11:46 +0100

    use directories
    Use OS-specific locations for the cache directory to make treefmt
    cross-platform.

--- src/command/format.rs
+++ src/command/format.rs
@@ -2,14 +2,23 @@ use crate::config;
 use crate::engine::run_treefmt;
 use crate::CLOG;
 use anyhow::anyhow;
+use directories::ProjectDirs;
 use std::fs;
-use std::path::Path;
-use std::{env, path::PathBuf};
+use std::path::PathBuf;
 
 pub fn format_cmd(work_dir: PathBuf, paths: Vec<PathBuf>) -> anyhow::Result<()> {
+    let proj_dirs = match ProjectDirs::from("com", "NumTide", "treefmt") {
+        Some(x) => x,
+        None => {
+            return Err(anyhow!(
+            "Could not find the project directories. On Unix, check if the HOME env is missing."
+        ))
+        }
+    };
+
     // Search for the treefmt.toml from there.
-    let treefmt_toml = match config::lookup(&work_dir) {
-        Some(p) => p,
+    let config_file = match config::lookup(&work_dir) {
+        Some(path) => path,
         None => {
             return Err(anyhow!(
                 "{} could not be found in {} and up. Use the --init option to create one.",
@@ -18,39 +27,21 @@ pub fn format_cmd(work_dir: PathBuf, paths: Vec<PathBuf>) -> anyhow::Result<()> 
             ))
         }
     };
-    // We assume that the parent always exists since the file is contained in a folder.
-    let treefmt_root = treefmt_toml.parent().unwrap();
 
-    CLOG.debug(&format!(
-        "Found {} at {}",
-        treefmt_toml.display(),
-        treefmt_root.display()
-    ));
-
-    // Find the location of the cache directory to store the eval-cache manifests.
-    let xdg_cache_dir = match env::var("XDG_CACHE_DIR") {
-        Ok(path) => path,
-        Err(err) => {
-            CLOG.debug(&format!("{}", err));
-            match env::var("HOME") {
-                Ok(h) => {
-                    let home_cache = Path::new(&h).join(".cache");
-                    CLOG.debug(&format!(
-                        "Set the $XDG_CACHE_DIR to {}",
-                        home_cache.display()
-                    ));
-                    home_cache.display().to_string()
-                }
-                Err(err) => return Err(anyhow!("cannot open HOME due to {}.", err)),
-            }
-        }
-    };
-    let cache_dir = Path::new(&xdg_cache_dir).join("treefmt/eval-cache");
+    let cache_dir = proj_dirs.cache_dir().join("eval-cache");
     // Make sure the cache directory exists.
     fs::create_dir_all(&cache_dir)?;
 
+    CLOG.debug(&format!(
+        "work_dir={} cache_dir={} config_file={} paths={:?}",
+        work_dir.display(),
+        cache_dir.display(),
+        config_file.display(),
+        paths
+    ));
+
     // Finally run the main formatter logic from the engine.
-    run_treefmt(&work_dir, &cache_dir, &treefmt_toml, &paths)?;
+    run_treefmt(&work_dir, &cache_dir, &config_file, &paths)?;
 
     Ok(())
 }
