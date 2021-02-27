a5f76ebc57fba95c9bb6907b2aca95943aa10c74
Jonas Chevalier <zimbatm@zimbatm.com>
2021-02-27 16:15:01 +0000

    cli: add --fail-on-change (#75)
    Make the CI scenario a bit easier to integrate.

--- ci.sh
+++ ci.sh
@@ -1,10 +1,16 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash
+# nix-shell loads the devshell making rust and all the formatters available.
+# shellcheck shell=bash
 set -euo pipefail
 
 set -x
 
-nix-shell --pure --run "cargo build"
+# Quick sanity check
+cargo test
 
-nix-shell --pure --run "cargo test"
+# Check that no code needs reformatting. Acts as a minimal integration test.
+cargo run -- --fail-on-change
 
+# Build the nix package
 nix-build -A defaultNix.defaultPackage.x86_64-linux
