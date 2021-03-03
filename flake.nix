ee8a3e54ea5b48de1940d1a6f77e8e1fa96a1ebf
Andika Demas Riyandi <andika.riyan@gmail.com>
2021-03-03 19:16:23 +0700

    build static binary for treefmt

--- flake.nix
+++ flake.nix
@@ -15,6 +15,13 @@
             inherit system;
             # Makes the config pure as well. See <nixpkgs>/top-level/impure.nix:
             config = { };
+            crossOverlays = [
+              (import "${nixpkgs}/pkgs/top-level/static.nix")
+            ];
+            crossSystem = {
+                isStatic = true;
+                config = "x86_64-unknown-linux-musl";
+            };
             overlays = [
               naersk.overlay
               devshell.overlay
@@ -24,6 +31,10 @@
         {
           defaultPackage = pkgs.naersk.buildPackage {
             src = self;
+            remapPathPrefix = true;
+            nativeBuildInputs = with pkgs.buildPackages; [ pkgconfig ];
+            CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+            CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${pkgs.buildPackages.llvmPackages_10.lld}/bin/lld";   
           };
 
           devShell = pkgs.devshell.fromTOML ./devshell.toml;
