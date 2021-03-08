aa84175b774a164e80ca4809eb8a41500ccb8cab
Andika Demas Riyandi <andika.riyan@gmail.com>
2021-03-08 15:12:59 +0700

    use rust-overlay

--- flake.nix
+++ flake.nix
@@ -6,8 +6,9 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.naersk.inputs.nixpkgs.follows = "nixpkgs";
   inputs.naersk.url = "github:nmattia/naersk";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
-  outputs = { self, nixpkgs, naersk, flake-utils, devshell }:
+  outputs = { self, nixpkgs, naersk, flake-utils, devshell, rust-overlay }:
     (
       flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
         let
@@ -15,32 +16,33 @@
             inherit system;
             # Makes the config pure as well. See <nixpkgs>/top-level/impure.nix:
             config = { };
-            crossOverlays = [
-              (import "${nixpkgs}/pkgs/top-level/static.nix")
-            ];
-            crossSystem = {
-                isStatic = true;
-                config = "x86_64-unknown-linux-musl";
-            };
             overlays = [
+              rust-overlay.overlay
               naersk.overlay
               devshell.overlay
             ];
           };
 
+          rust = pkgs.rust-bin.stable.latest.rust.override {
+            extensions = [
+              "clippy-preview"
+              "rustfmt-preview"
+              "rust-analysis"
+              "rust-std"
+              "rust-src"
+            ];
+            targets = [
+              "wasm32-unknown-unknown"
+              "x86_64-unknown-linux-musl"
+            ];
+          };
+
           treefmt = (pkgs.naersk.override {
-              rustc = pkgs.rustPackages.rustc;
-              cargo = pkgs.rustPackages.cargo;
-            }).buildPackage {
-              src = self;
-              remapPathPrefix = true;
-              nativeBuildInputs = with pkgs.buildPackages; [ pkgconfig ];
-              PKG_CONFIG_ALLOW_CROSS = "true";
-              PKG_CONFIG_ALL_STATIC = "true";
-              LIBZ_SYS_STATIC = "1";
-              CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-              CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${pkgs.buildPackages.llvmPackages_10.lld}/bin/lld";
-            };
+            rustc = rust;
+          }).buildPackage {
+            src = self;
+            CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          };
         in
         {
           # What is used when invoking `nix run github:numtide/treefmt`
