26d7f5f15e5d22d4413ba7c5ae0447fdc0b5cc76
Jonas Chevalier <zimbatm@zimbatm.com>
2021-12-24 17:47:18 +0100

    website: remove landing page (#145)
    It's better to have no landing page than a half-finished one.

--- docs/default.nix
+++ docs/default.nix
@@ -6,10 +6,6 @@ stdenv.mkDerivation {
   src = ./.;
   buildPhase = "mdbook build";
   installPhase = ''
-    mkdir $out
-    # Move the landing page as the main HTML. Docbook copies all of the files
-    # so it can't be named index.html
-    mv landing.html $out/index.html
-    mv book $out/docs
+    mv book $out
   '';
 }
