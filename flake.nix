becf50f2ff695b70561a9560a36f933bb2594378
Sridhar Ratnakumar <srid@srid.ca>
2022-08-23 11:08:12 -0400

    flake-parts: use the same nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
   # To update all inputs:
   # $ nix flake update --recreate-lock-file
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.flake-parts.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, flake-parts }@inputs:
     flake-parts.lib.mkFlake { inherit self; } {
