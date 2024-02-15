d53f98ea051105f2456fc951109ab1ccfc2da611
Brian McGee <brian@bmcgee.ie>
2024-02-15 09:43:16 +0000

    feat: support --version
    Signed-off-by: Brian McGee <brian@bmcgee.ie>

---
+++ build/build.go
@@ -0,0 +1,6 @@
+package build
+
+var (
+	Name    = "treefmt"
+	Version = "v0.0.1+dev"
+)
--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	"git.numtide.com/numtide/treefmt/build"
 	"git.numtide.com/numtide/treefmt/cli"
 	"github.com/alecthomas/kong"
 )
 
 func main() {
+	// This is to maintain compatibility with 1.0.0 which allows specifying the version with a `treefmt --version` flag
+	// on the 'default' command. With Kong it would be better to have `treefmt version` so it would be treated as a
+	// separate command. As it is, we would need to weaken some of the `existingdir` and `existingfile` checks kong is
+	// doing for us in the default format command.
+	for _, arg := range os.Args {
+		if arg == "--version" || arg == "-V" {
+			fmt.Printf("%s %s\n", build.Name, build.Version)
+			return
+		}
+	}
+
 	ctx := kong.Parse(&cli.Cli)
 	ctx.FatalIfErrorf(ctx.Run())
 }
