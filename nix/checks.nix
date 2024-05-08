ebfd17b76a9a51b59217bce399138aaa1e85a603
Brian McGee <brian@bmcgee.ie>
2024-05-08 08:54:58 +0100

    feat: add nix github workflow
    Signed-off-by: Brian McGee <brian@bmcgee.ie>

--- nix/checks.nix
+++ nix/checks.nix
@@ -1,5 +1,16 @@
-{lib, ...}: {
+{
+  lib,
+  inputs,
+  self,
+  ...
+}: {
+  # generate github actions matrix using the flake's checks
+  flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
+    checks = lib.getAttrs ["x86_64-linux" "x86_64-darwin"] self.checks;
+  };
+
   perSystem = {self', ...}: {
+    # mixin every package
     checks = with lib; mapAttrs' (n: nameValuePair "package-${n}") self'.packages;
   };
 }
