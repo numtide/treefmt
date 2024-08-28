611cad76375da34f19c4c98c8affb23ecb0bc924
zimbatm <zimbatm@zimbatm.com>
2024-08-28 14:45:15 +0200

    chore(gomod2nix): add breadcrumb
    Make it easier for developers to find out how to update the file.

--- nix/devshells/renovate.nix
+++ nix/devshells/renovate.nix
@@ -10,7 +10,12 @@ pkgs.mkShellNoCC {
     (pkgs.writeShellApplication {
       name = "gomod2nix:update";
       runtimeInputs = [pkgs.git perSystem.gomod2nix.default];
-      text = "gomod2nix --outdir nix/packages/treefmt";
+      text = ''
+        set -eu
+        gomod2nix --outdir nix/packages/treefmt
+        # shellcheck disable=SC2016
+        sed -i '1i # Generated with `nix develop .#renovate -c gomod2nix:update`' nix/packages/treefmt/gomod2nix.toml
+      '';
     })
   ];
 }
