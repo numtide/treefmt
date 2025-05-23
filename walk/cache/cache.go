8c251487657316f31f89a03566a598a537232105
Brian McGee <brian@bmcgee.ie>
2025-05-23 11:09:15 +0100

    fix: force-remove db file when clearing cache
    It also improves logging of cache file location when the db cannot be opened.
    
    Fixes #592
    
    Signed-off-by: Brian McGee <brian@bmcgee.ie>

--- walk/cache/cache.go
+++ walk/cache/cache.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/adrg/xdg"
@@ -14,26 +15,33 @@ const (
 	bucketPaths = "paths"
 )
 
-func Open(root string) (*bolt.DB, error) {
-	var (
-		err  error
-		path string
-	)
-
-	// Otherwise, the database will be located in `XDG_CACHE_DIR/treefmt/eval-cache/<name>.db`, where <name> is
-	// determined by hashing the treeRoot path.
-	// This associates a given treeRoot with a given instance of the cache.
+// Path returns a unique local cache file path for the given root string, using its SHA-256 hash.
+func Path(root string) (string, error) {
 	digest := sha256.Sum256([]byte(root))
 
 	name := hex.EncodeToString(digest[:])
-	if path, err = xdg.CacheFile(fmt.Sprintf("treefmt/eval-cache/%v.db", name)); err != nil {
-		return nil, fmt.Errorf("could not resolve local path for the cache: %w", err)
+
+	path, err := xdg.CacheFile(fmt.Sprintf("treefmt/eval-cache/%v.db", name))
+	if err != nil {
+		return "", fmt.Errorf("could not resolve local path for the cache: %w", err)
+	}
+
+	return path, nil
+}
+
+// Open initialises and opens a Bolt database for the specified root path.
+// Returns a pointer to the opened database or an error if initialisation fails.
+func Open(root string) (*bolt.DB, error) {
+	// determine the db location
+	path, err := Path(root)
+	if err != nil {
+		return nil, err
 	}
 
 	// open db
 	db, err := bolt.Open(path, 0o600, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
-		return nil, fmt.Errorf("failed to open cache db: %w", err)
+		return nil, fmt.Errorf("failed to open cache db at %s: %w", path, err)
 	}
 
 	// ensure bucket exist
@@ -56,28 +64,19 @@ func PathsBucket(tx *bolt.Tx) *bolt.Bucket {
 	return tx.Bucket([]byte("paths"))
 }
 
-func deleteAll(bucket *bolt.Bucket) error {
-	c := bucket.Cursor()
-	for k, v := c.First(); !(k == nil && v == nil); k, v = c.Next() {
-		if err := c.Delete(); err != nil {
-			return fmt.Errorf("failed to remove cache entry for key %s: %w", string(k), err)
-		}
+func Remove(root string) error {
+	// determine the db location
+	path, err := Path(root)
+	if err != nil {
+		return err
 	}
 
-	return nil
-}
-
-func Clear(db *bolt.DB) error {
-	err := db.Update(func(tx *bolt.Tx) error {
-		err := deleteAll(PathsBucket(tx))
-		if err != nil {
-			return fmt.Errorf("failed to clear cache: %w", err)
-		}
-
-		return nil
-	})
-	if err != nil {
-		return fmt.Errorf("failed to clear cache: %w", err)
+	// Remove any db which might already exist.
+	// If a treefmt process is currently running with a db open at the same location, it will continue to function
+	// as normal, however, when it exits the disk space its inode was referencing will be reclaimed.
+	// This will not work on Windows if we ever support it.
+	if err = os.Remove(path); !(err == nil || os.IsNotExist(err)) {
+		return fmt.Errorf("failed to remove cache db at %s: %w", path, err)
 	}
 
 	return nil
